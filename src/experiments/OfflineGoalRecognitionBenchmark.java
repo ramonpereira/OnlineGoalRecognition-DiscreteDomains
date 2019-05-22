2947f0933e6f7a7078c6941c6d91adeaa8d9e76e
Ramon Fraga Pereira <ramonfpereira@gmail.com>
2019-05-22 18:56:08 -0300

    Adding results with respect to the offline mode of Mirroring with Landmarks.

--- src/experiments/OfflineGoalRecognitionBenchmark.java
+++ src/experiments/OfflineGoalRecognitionBenchmark.java
@@ -15,8 +15,8 @@ public class OfflineGoalRecognitionBenchmark {
 		
 		String contentFile = "Obs % \tAccuracy \tPrecision \tRecall \tF1-score \tFall-out \tMiss-rate \tAvg. Goals \tAvg. Observations \tRecognized Goals \tTime(s) \n";
 		float totalProblems = 0;
-		for(int obs=10; obs<=100; obs+=20){
-			if(obs > 70)
+		for(int obs=25; obs<=100; obs+=25){
+			if(obs > 75)
 				obs = 100;
 			
 			String observability = String.valueOf(obs);
